eeb560b951c03c455cbda32442a2dd10320a5069
Nobyl <kevin.kennard2013@gmail.com>
2022-04-18 18:16:42 -0400

    Add basic mathematical functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const addition = function (num1, num2) {
+    return (num1 + num2);
+}
+
+const subtraction = function (num1, num2) {
+    return (num1 - num2);
+}
+
+const multiplication = function (num1, num2) {
+    return (num1 * num2);
+}
+
+const division = function (num1, num2) {
+    return (num1 / num2);
+}
