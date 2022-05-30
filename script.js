b5514db24616b4e1cab2d7f7d7fa9b03b9f447b6
Nobyl <kevin.kennard2013@gmail.com>
2022-05-30 16:12:32 -0400

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ const multiplication = function (num1, num2) {
 const division = function (num1, num2) {
     return (num1 / num2);
 }
+
+const operate = function(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return addition(num1, num2);
+        case "-":
+            return subtraction(num1, num2);
+        case "*":
+            return multiplication(num1, num2);
+        case "/":
+            return division(num1, num2);
+    }
+}
